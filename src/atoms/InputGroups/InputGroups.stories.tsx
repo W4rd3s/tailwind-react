cd07bbaaf3d15f313a429fd7e81d62cf295a11a8
W4rd3s <thomas.blavot@gmail.com>
2023-02-02 00:27:19 +0100

    feat: add component InputGroups

---
+++ src/atoms/InputGroups/InputGroups.stories.tsx
@@ -0,0 +1,16 @@
+import InputGroups from './InputGroups';
+
+export default {
+  title: 'Atoms/InputGroups',
+  component: InputGroups,
+};
+
+function Template(args) {
+  return <InputGroups {...args} />;
+}
+
+export const Default = Template.bind({});
+
+Default.args = {
+  className: '',
+};
